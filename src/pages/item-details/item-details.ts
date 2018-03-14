3b678dcfc486adb19eb789e0ea1ad2c2c2dbcda3
leiming <2008weileiming@gmail.com>
2018-03-14 10:55:17 +0800

    Reset Project

--- src/pages/item-details/item-details.ts
+++ src/pages/item-details/item-details.ts
@@ -11,31 +11,9 @@ import { ActionSheetController } from 'ionic-angular'
 export class ItemDetailsPage {
   selectedItem: any;
 
-  constructor(public navCtrl: NavController,
-              public navParams: NavParams,
-              public actionsheetCtrl: ActionSheetController) {
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
     // If we navigated to this page, we will have an item available as a nav param
     this.selectedItem = navParams.get('item');
-  }
-
-  presentActionSheet() {
-    let buttons = [
-      {
-        text: 'Destructive',
-        role: 'destructive',
-        handler: () => {
-          console.log('Destructive clicked');
-        }
-      },
-      {
-        text: 'Archive',
-        handler: () => {
-          console.log('Archive clicked')
-        }
-      }
-    ];
-    let actionSheet = this.actionsheetCtrl.create({title: 'leiming', buttons: buttons});
-    actionSheet.present();
   }
 
 }
