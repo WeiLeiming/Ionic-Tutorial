31bdf4f50cd1d2d4aba665979afc627a00ea5612
leiming <2008weileiming@gmail.com>
2018-03-16 12:02:19 +0800

    Update action-sheets

--- src/pages/action-sheets/action-sheets.ts
+++ src/pages/action-sheets/action-sheets.ts
@@ -1,15 +1,32 @@
 import { Component } from '@angular/core';
 
-import { ActionSheetController, Platform } from 'ionic-angular';
+import { NavController, ActionSheetController, Platform } from 'ionic-angular';
+
+import { HelloIonicPage } from "../hello-ionic/hello-ionic";
 
 @Component({
-  selector: 'action-sheets-basic-page',
+  selector: 'page-action-sheets-basic',
   templateUrl: 'action-sheets.html'
 })
 
 export class ActionSheetsPage {
 
-  constructor(public actionSheetCtrl: ActionSheetController, public platform: Platform) {
+  data:any = [
+    {name:'托尼·史塔克', birthYear:1970, gender:1, description:'钢铁侠'},
+    {name:'史蒂夫·罗杰斯', birthYear:1920, gender:1, description:'美国队长'},
+    {name:'娜塔莎罗·曼诺夫', birthYear:1928, gender:0, description:'黑寡妇'},
+    {name:'索尔', birthYear:-5555,gender:1, description:'雷神'},
+    {name:'克林顿·巴顿', birthYear:1974, gender:1, description:'鹰眼'},
+    {name:'布鲁斯·班纳', birthYear:1975, gender:1, description:'绿巨人浩克'}
+  ];
+
+  name: string
+
+  constructor(public navCtrl: NavController, public actionSheetCtrl: ActionSheetController, public platform: Platform) {
+  }
+
+  pushNextPage() {
+    this.navCtrl.push(HelloIonicPage)
   }
 
   presentActionSheet() {
